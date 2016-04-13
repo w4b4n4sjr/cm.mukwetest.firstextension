5616380b122a7ae2c4a2ecc4eb656dc5cfc21e75
w4b4n4sjr <w4b4n4sjr@gmail.com>
2016-04-13 01:13:14 +0100

    local directory that points to github repo has been modified

--- CRM/Firstextension/Page/Meetings.php
+++ CRM/Firstextension/Page/Meetings.php
@@ -3,12 +3,21 @@
 require_once 'CRM/Core/Page.php';
 
 class CRM_Firstextension_Page_Meetings extends CRM_Core_Page {
+
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('Meetings'));
 
     // Example: Assign a variable for use in a template
     $this->assign('currentTime', date('Y-m-d H:i:s'));
+    //Assign a contact for use in a template
+    $params = array('do_not_email' => 0, );
+
+    $result = civicrm_api3('contact', 'get', $params);
+
+    $this->assign('contactGotten', $result['count']);
+
+    $this->assign('contacts', $result);
 
     parent::run();
   }
